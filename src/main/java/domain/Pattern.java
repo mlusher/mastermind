81cc2efc5a5229254a8eb9714cf588348ce16058
Megan Lusher <megan.lusher@gmail.com>
2013-10-31 21:49:24 -0400

    extracting out color and pattern logic

--- src/main/java/domain/Pattern.java
+++ src/main/java/domain/Pattern.java
@@ -1,14 +1,22 @@
 package domain;
 
+import com.google.common.base.Function;
+import com.google.common.base.Predicate;
+import com.google.common.collect.Collections2;
+import com.google.common.collect.Lists;
+
 import java.util.ArrayList;
+import java.util.Collection;
 import java.util.List;
 
+import static com.google.common.collect.Collections2.filter;
+import static com.google.common.collect.Collections2.transform;
 import static domain.Key.COLOR;
 import static domain.Key.POSITION;
 import static java.util.Arrays.asList;
 
 public class Pattern {
-    private List pegs;
+    private List<Peg> pegs;
 
     public Pattern(Peg... pegs) {
         this.pegs = asList(pegs);
@@ -19,32 +27,78 @@ public class Pattern {
     }
 
     public List<Key> match(Pattern pattern) {
-        ArrayList<Key> positionKeys = new ArrayList<Key>();
-
-        List<Peg> pegsToRemove = new ArrayList<Peg>();
         List<Peg> inputPegs = pattern.getPegs();
+
+
+        Tuple<List<Key>, List<Peg>> positionMatches = extractPositionMatches(inputPegs);
+        inputPegs.removeAll(positionMatches.getTwo());
+        ArrayList<Key> colorKeys = extractColorKeys(inputPegs);
+
+
+        ArrayList<Key> matchAnswer = new ArrayList<Key>();
+        matchAnswer.addAll(positionMatches.getOne());
+        matchAnswer.addAll(colorKeys);
+        return matchAnswer;
+    }
+
+    private ArrayList<Key> extractColorKeys(List<Peg> inputPegs) {
+
+        Predicate<Peg> pegExistsInPatternPegs = new Predicate<Peg>() {
+            @Override
+            public boolean apply(Peg input) {
+                return pegs.contains(input);
+            }
+        };
+        Collection<Peg> matchingPegs = filter(inputPegs, pegExistsInPatternPegs);
+
+        Function<Peg, Key> colorKeyForEveryInputPeg = new Function<Peg, Key>() {
+            @Override
+            public Key apply(Peg peg) {
+                return COLOR;
+            }
+        };
+        Collection<Key> colorKeys = transform(matchingPegs, colorKeyForEveryInputPeg);
+
+        return new ArrayList<Key>(colorKeys);
+    }
+
+    private Tuple<List<Key>, List<Peg>> extractPositionMatches(List<Peg> inputPegs) {
+
+
+        List<Key> positionMatches = new ArrayList<Key>();
+        List<Peg> pegMatches = new ArrayList<Peg>();
+
         for (int x = 0; x < inputPegs.size(); x++) {
-            if (inputPegs.get(x) == pegs.get(x)){
-                positionKeys.add(POSITION);
-                pegsToRemove.add(inputPegs.get(x));
+            if (inputPegs.get(x) == pegs.get(x)) {
+                positionMatches.add(POSITION);
+                pegMatches.add(inputPegs.get(x));
             }
         }
 
-        List<Peg> pegs1 = pattern.getPegs();
-        pegs1.removeAll(pegsToRemove);
-
-        ArrayList<Key> colorKeys = new ArrayList<Key>();
-        for (Peg peg : pegs1) {
-            if (pegs.contains(peg))
-                colorKeys.add(COLOR);
-        }
-        List<Key> colors = colorKeys;
-
-        positionKeys.addAll(colors);
-        return positionKeys;
+        return new Tuple<List<Key>, List<Peg>>(positionMatches, pegMatches);
     }
 
-    public static Pattern p(Peg... pegs) {
+
+    public static Pattern pattern(Peg... pegs) {
         return new Pattern(pegs);
+    }
+
+}
+
+class Tuple<One, Two> {
+    private final One one;
+    private final Two two;
+
+    Tuple(One one, Two two) {
+        this.one = one;
+        this.two = two;
+    }
+
+    One getOne() {
+        return one;
+    }
+
+    Two getTwo() {
+        return two;
     }
 }
--- src/test/java/TestPattern.java
+++ src/test/java/TestPattern.java
@@ -3,9 +3,11 @@ import org.junit.Test;
 
 import static domain.Key.COLOR;
 import static domain.Key.POSITION;
-import static domain.Pattern.p;
+import static domain.Pattern.pattern;
 import static domain.Peg.*;
 import static java.util.Arrays.asList;
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.MatcherAssert.assertThat;
 import static org.junit.Assert.assertEquals;
 
 public class TestPattern {
@@ -18,28 +20,19 @@ public class TestPattern {
 
     @Test
     public void matchesPegPosition() {
-        Pattern pattern = new Pattern(BLUE, GREEN, RED, WHITE);
-        assertEquals(
-                asList(POSITION, POSITION, POSITION, POSITION),
-                pattern.match(pattern)
-        );
+        assertThat(pattern(BLUE, GREEN, RED, WHITE).match(pattern(BLUE, GREEN, RED, WHITE)),
+                is(asList(POSITION, POSITION, POSITION, POSITION)));
     }
 
     @Test
     public void matchesTwoPositionsAndNoColors() {
-        Pattern pattern = new Pattern(BLUE, GREEN, RED, WHITE);
-        assertEquals(
-                asList(POSITION, POSITION),
-                pattern.match(p(BLUE, GREEN, YELLOW, YELLOW))
-        );
+        assertThat(pattern(BLUE, GREEN, RED, WHITE).match(pattern(BLUE, GREEN, YELLOW, YELLOW)),
+                is(asList(POSITION, POSITION)));
     }
 
     @Test
     public void matchesPegColors() {
-        Pattern pattern = new Pattern(BLUE, GREEN, YELLOW, WHITE);
-        assertEquals(
-                asList(COLOR, COLOR, COLOR, COLOR),
-                pattern.match(p(GREEN, WHITE, BLUE, YELLOW))
-        );
+        assertThat(pattern(BLUE, GREEN, YELLOW, WHITE).match(pattern(GREEN, WHITE, BLUE, YELLOW)),
+                is(asList(COLOR, COLOR, COLOR, COLOR)));
     }
 }
